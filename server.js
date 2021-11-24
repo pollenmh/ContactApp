a5c519f6363963a53cd4f13550e18b92e478228f
pollenmh@gmail.com <pollenmh@gmail.com>
2021-11-24 11:19:37 +0000

    added process.env.port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsD
 app.set('view engine', 'hbs');
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Express server listening on port 3000");
 });
 
 app.use('/contact', contactController);
